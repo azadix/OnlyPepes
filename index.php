569a9239a33fcf1adcd9101260f71869243786c4
azadix <bartosz@madalski.pl>
2022-02-11 20:52:47 +0100

    Added hamburger, media queries and renaming

--- index.php
+++ index.php
@@ -13,10 +13,10 @@
     $files = $connection->query("SELECT `name`,`fileName`,`extension` FROM `pepelist`");
     
     foreach ($files as $pepe){
-        echo "<div class='pepe'>";
+        echo "<div class='divPepe'>";
         echo "<h2>{$pepe['name']}</h2>";
         echo "<img src='pepes/{$pepe['fileName']}.{$pepe['extension']}' class='imagePepe' />";
-        echo "<hr style='height:1px;border-width:0;color:gray;background-color:gray'/>";
+        echo "<hr class='break'/>";
         echo "</div>";
     };
 
